fb521fc4aec58b1e027f0581725bd6ae25fac100
animationarmufa-oss <animationarmufa@gmail.com>
2026-01-14 13:34:52 +0700

    Remove sensitive token entries from token.js
    Removed multiple token entries for security reasons.

--- token.js
+++ token.js
@@ -8,16 +8,6 @@
     "group_link": "https://https://t.me/armufastorepanel"
   },
   "tokens": [
-    "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM",
-    "8502995694:AAHocXIJc0wJKOo29h9bLFiHjIUVS06NhN0",
-    "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
-    "8516015248:AAEBV_UCmAb7zJc61jLCImg_Z4dHtusanjY",
-    "8555248193:AAGURW7yhgL-DftzDbdDsapqEVFe9fvKfEo",
-    "8591404472:AAHAaJPkVTZ91Zs6g8uKiHnOQLgSgfEltHA",
-    "8343245529:AAHUJUlPB9R0sI8hRdwzO55cx9pPmgoyWrQ",
-    "8249587853:AAHTYkN-Ndkg8VLic-KdZGxRf9NZ1Fe3DyY",
-    "8481404836:AAFj2YfOxAJUqwaW46ETrWk43fMtR1oND6g",
-    "8332371133:AAHqkVT0ACoEOfogrlT7t2MjlOjx-pcorV8",
     "8323261136:AAEawQs9NB_26SGFW7ZKcUfpW3pzxM1_tsk"
   ],
   "sha": "462d803d6ba16b4ea8c80ae4b9741ae4e54bb2c3",
