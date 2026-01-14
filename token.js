9802f974d14950b5f8501a2467f641a4dd0be8fa
animationarmufa-oss <animationarmufa@gmail.com>
2026-01-14 13:17:51 +0700

    Add token configuration and buyer list

---
+++ token.js
@@ -0,0 +1,68 @@
+{
+  "owners": [
+    "7973092286"
+  ],
+  "config": {
+    "channel_id": "@realarmufastore",
+    "group_id": "-1003496764738",
+    "group_link": "https://https://t.me/armufastorepanel"
+  },
+  "tokens": [
+    "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM",
+    "8502995694:AAHocXIJc0wJKOo29h9bLFiHjIUVS06NhN0",
+    "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
+    "8516015248:AAEBV_UCmAb7zJc61jLCImg_Z4dHtusanjY",
+    "8555248193:AAGURW7yhgL-DftzDbdDsapqEVFe9fvKfEo",
+    "8591404472:AAHAaJPkVTZ91Zs6g8uKiHnOQLgSgfEltHA",
+    "8343245529:AAHUJUlPB9R0sI8hRdwzO55cx9pPmgoyWrQ",
+    "8249587853:AAHTYkN-Ndkg8VLic-KdZGxRf9NZ1Fe3DyY",
+    "8481404836:AAFj2YfOxAJUqwaW46ETrWk43fMtR1oND6g",
+    "8332371133:AAHqkVT0ACoEOfogrlT7t2MjlOjx-pcorV8",
+    "8323261136:AAEawQs9NB_26SGFW7ZKcUfpW3pzxM1_tsk"
+  ],
+  "sha": "462d803d6ba16b4ea8c80ae4b9741ae4e54bb2c3",
+  "list_buyer": [
+    {
+      "id": "6851921545",
+      "name": "Xven`zo 㕠",
+      "username": "Xvenzo",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7726353539",
+      "name": "- z for?",
+      "username": "ZyyTsune",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8064092635",
+      "name": "×͜× || 尺乇乙乙一緒",
+      "username": "ziistr",
+      "role": "ᴏᴡɴᴇƦ"
+    },
+    {
+      "id": "7476194064",
+      "name": "『𝗥𝟴』𝙇𝘼𝙉𝙂𝙕𝙕交",
+      "username": "Langzz31x",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "1619141907",
+      "name": "alwaysfazz (limit)",
+      "username": "fazzstudent",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7720022578",
+      "name": "𝑅𝑒𝑧𝑖",
+      "username": "Alwaysrezz",
+      "role": "ƦᴇSSᴇʟᴇƦ"
+    },
+    {
+      "id": "7973092286",
+      "name": "⏤͟͟͞͞ARMUFA STORE⏤͟͟͞͞⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ",
+      "username": "MYARMUFASTORE",
+      "role": "ᴏᴡɴᴇƦ"
+    }
+  ]
+}
